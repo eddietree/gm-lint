a02461a8f0bd18b713e45ff4c559ad0e1427cdca
eddietree <eddie@illogictree.com>
2013-09-08 01:19:12 +0900

    more cleaning up of code

--- gm_lint.py
+++ gm_lint.py
@@ -5,11 +5,39 @@ import subprocess, os, re
 
 class GmLintCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+	def highlight_line(self, line_number):
+
+		# grabs region that has the compile error
+		line_pt = self.view.text_point(line_number-1, 0)
+		line_region = self.view.line(line_pt)
+		compile_error_regions = [line_region]
+
+		# highlight the region
+		self.view.add_regions("compile_error_regions", compile_error_regions, "comment", "bookmark",  sublime.DRAW_NO_FILL )
+		self.view.show_at_center( line_region )
+
+		# goto error line
+		self.view.sel().clear()
+		self.view.sel().add(sublime.Region(line_pt))
+		self.view.show(line_pt)
+
+	def reset(self):
 
 		# erase previous status and regions
 		self.view.erase_status("gm-lint")   	
 		self.view.erase_regions("compile_error_regions")
+
+	def handle_compile_fail(self, line_number, compile_error_msg):
+
+		output_msg = "[gm-lint] Compile error: Line " + str(line_number) + " - " + compile_error_msg
+		print ( output_msg )
+
+		self.view.set_status("gm-lint", output_msg)
+		self.highlight_line(line_number)
+
+	def run(self, edit):
+
+		self.reset()
 
 		# find the fullpath of the gm byte code exe
 		plugin_fullpath = os.path.realpath(__file__)
@@ -21,19 +49,16 @@ class GmLintCommand(sublime_plugin.TextCommand):
 		
 		# generate the string to be executed
 		exe_string =  "\"" + gm_exe_path + "\" -i " + "\"" + script_filename + "\""
-		foo = subprocess.Popen(exe_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		process = subprocess.Popen(exe_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 		# wait for compile to finish and listen to emitted return code
-		foo.wait()
-		return_code = foo.returncode
+		process.wait()
+		return_code = process.returncode
 
-		# if error code
-		if return_code == 1:
+		if return_code == 1: # if error in compiling
 
-			# show the console if error
-			#sublime.active_window().run_command("show_panel", {"panel": "console", "toggle": True})
-
-			output_bytes = foo.stdout.read()
+			# read the stdout
+			output_bytes = process.stdout.read()
 			output_string = output_bytes.decode("utf-8")
 			#print( "output: " + output_string)
 
@@ -42,44 +67,26 @@ class GmLintCommand(sublime_plugin.TextCommand):
 			match = pattern.match( output_string )
 			line_number = int(match.group(1))
 			compile_error_msg = match.group(2)
-			
-			#print( "LINE: " + str(line_number))
-			#print( "ERROR: " + compile_error_msg)
 
-			# set the status
-			output_msg = "[gm-lint] Compile error: Line " + str(line_number) + " - " + compile_error_msg
-			print ( output_msg )
-			self.view.set_status("gm-lint", output_msg)
+			self.handle_compile_fail( line_number, compile_error_msg )			
 
-			# grabs region that has the compile error
-			line_pt = self.view.text_point(line_number-1, 0)
-			line_region = self.view.line(line_pt)
-			compile_error_regions = [line_region]
-
-			# highlight the region
-			self.view.add_regions("compile_error_regions", compile_error_regions, "comment", "bookmark",  sublime.DRAW_NO_FILL )
-			self.view.show_at_center( line_region )
-
-			# goto error line
-			self.view.sel().clear()
-			self.view.sel().add(sublime.Region(line_pt))
-			self.view.show(line_pt)
-		
-		else:
+		else: # success
 
 			print ("[gm-lint] Successfully compiled '" + script_filename + "'!")
 			self.view.set_status("gm-lint", "[gm-lint] Compiled success")
 
-  
+
 class ListenSaveGameMonkeyFile(sublime_plugin.EventListener):
+
     def on_post_save(self, view):
 
-    	# what operating system?
-    	os_platform = sublime.platform()
+    	# this plugin only works on windows!
+    	if sublime.platform() != "windows":
+    		print ("gm-lint works only on Windows!")
 
     	# get file extension of the file we just saved
     	extension = os.path.splitext(view.file_name())[1] 
 
     	# run only on .gm files and on windows
-    	if extension == ".gm" and os_platform == "windows" :
+    	if extension == ".gm":
     		view.run_command("gm_lint")
